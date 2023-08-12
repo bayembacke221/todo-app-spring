c29471c2f481a4b9d6c8b33df6969e4dbed6bfd0
bayembacke221 <mbackembaye74@gmail.com>
2023-08-12 00:52:13 +0000

    add test

--- src/test/java/sn/bayembacke/todo/TodoApplicationTests.java
+++ src/test/java/sn/bayembacke/todo/TodoApplicationTests.java
@@ -1,13 +1,23 @@
 package sn.bayembacke.todo;
 
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+
+import org.springframework.context.ApplicationContext;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 @SpringBootTest
+@ActiveProfiles("test")
+
 class TodoApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads(ApplicationContext context) {
+		assertThat(context).isNotNull();
 	}
+
+
 
 }
